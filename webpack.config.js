ccf3948bb55bb455de82c54325d76fbeba5577ee
mauricio moitino <maurimoitii@gmail.com>
2023-09-17 00:24:14 -0300

    version funcionando en dist, a pesar de presentar errores en console

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
     const HtmlWebpackPlugin = require('html-webpack-plugin');
 
     module.exports = {
-    entry:  '/js/main.js',
+    entry:  './js/main.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
@@ -24,10 +24,10 @@
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
         },
-        {
-            test: /\.html$/,
-            use: 'html-loader'
-        },
+        // {
+        //     test: /\.html$/,
+        //     use: 'html-loader'
+        // },
         {
             test: /\.(png|jpg|gif|svg)$/,
             use: {
@@ -41,7 +41,7 @@
     ]},
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html',
+            template: './index.html',
     }), 
     new webpack.ProvidePlugin({
         $: 'jquery',
