64e4a3ddb54d3bee88f219e350383dadb60997f8
mauricio moitino <maurimoitii@gmail.com>
2023-09-21 00:04:10 -0300

    testeando chart

--- src/components/applications/applications.js
+++ src/components/applications/applications.js
@@ -1,10 +1,13 @@
 
 const applications = () => {
-    $("main").load("../src/components/applications/applications.html", () => {
-        $("main").append($("<div>").load("../src/components/applications/chart-applications.html", () => {
-            initializeChart()
-        }));
-    }).show()
-}
+        $("main").load("../src/components/applications/applications.html", () => {
+            setTimeout(() => {
+            $("main").append($("<div>").load("../src/components/applications/chart-applications.html", () => {
+                initializeChart()
+            }));
+        }, 500)
+        }).show()
+    }
+
 
 export { applications }; 
