4f1612bff4ce9f9ef4d9555bb0b50c39b77b3a3d
mauricio moitino <maurimoitii@gmail.com>
2023-10-03 23:41:19 -0300

    v

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+import '../src/assets/css/main.css';
+import '../src/assets/css/normalize.css';
+import '../src/assets/css/responsive.css';
+import '../src/assets/css/responsive_ipad.css';
+import '../src/components/applications/applications.css';
+import '../src/components/sustainability/sustainability.css';
+import '../src/components/technology/technology.css';
+import '../src/components/about-us/about-us.css';
+
+
 import $ from 'jquery';
 import { header} from '../src/components/header/header.js';
 import { footer } from '../src/components/footer/footer.js';
