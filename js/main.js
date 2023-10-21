2b6ae0264a147479fce3b253a6e6642d24624dea
mauricio moitino <maurimoitii@gmail.com>
2023-10-20 22:07:53 -0300

    se implemento una 'fachada' para el iframe del footer, con la finalidad de mejorar rendimiento

--- js/main.js
+++ js/main.js
@@ -46,4 +46,16 @@ footer();
             hideContent();
             sustainability();  
         });
+        
+        $("footer").on('click', '.youtube-facade', function() {
+            var $this = $(this);
+            var videoId = $this.data('video-id');
+            var iframeSrc = 'https://www.youtube.com/embed/' + videoId + '?autoplay=1';
+    
+            // img hidden 
+            $this.find('.youtube-thumbnail').hide();
+    
+            // Set the src of iframe and display it
+            $this.find('iframe').attr('src', iframeSrc).show();
+        });
     }))
